18da18c9f89e0508dd541fb9c483d277db9ce554
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-07-12 15:06:02 -0600

    add comment

--- lib/api.rb
+++ lib/api.rb
@@ -1,21 +1,24 @@
-#get data from the inernate using scraping or hitting an API
+#get data from the inernate hitting an API
 class API
     BASE_URL = "https://calendarific.com/api/v2/holidays?&api_key=#{ENV["HOLIDAY_API_KEY"]}&country=US&year=2020"
     
     def get_holiday_info
         respo = HTTParty.get(BASE_URL)
         
-        #make request to the world holidays  
+        #Store the API Data in to an holi_arr variable   
         holi_arr = respo["response"]["holidays"]
+
+        #Iterate through to the world holidays api 
            holi_arr.each do |response_hash|                     
                       holi_hash = {
                         country: response_hash["country"]["name"],
                         name: response_hash["name"],
                         date: response_hash["date"]["iso"],
                         description: response_hash["description"]                         
-                      }                     
+                      }  
+    #create new Holiday object and stored 
     Holiday.new(holi_hash)
-    #binding.pry
+   
          end
          
     end
