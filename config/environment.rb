60683e5a5f5267294a2e340101f10338ca8a68f2
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-07-12 15:02:53 -0600

    add comment

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
+#Add Dependecy of teh Bundler and devenv
 require 'bundler/setup'
 require 'dotenv/load' 
 Bundler.require
 
-
+#route to the class
 require_relative "../lib/api"
 require_relative "../lib/cli"
 require_relative "../lib/holiday.rb"
